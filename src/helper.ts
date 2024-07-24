2df8483460b535f3aa703bcf25ebb498dfda9f7d
Sidharth <sidharth@vizbee.tv>
2024-07-24 23:15:40 +0530

    Fixed console log showing are package release

--- src/helper.ts
+++ src/helper.ts
@@ -1,8 +1,4 @@
 // Wrapper function for console.log
 export function log(...args: any[]) {
-  const isProduction = process.env.NODE_ENV === "production";
-
-  if (!isProduction) {
-    console.log(...args);
-  }
+  console.log(...args);
 }
